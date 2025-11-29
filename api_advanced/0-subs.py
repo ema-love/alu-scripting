099a19ec39bc945942afad5006d070d1d447300f
Erioluwa <e.akintayo@alustudent.com>
2025-11-29 21:54:45 +0200

    #

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""
-Module to query Reddit API for subreddit subscribers
-"""
+"""Module to query Reddit API for subreddit subscribers."""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit.
-    
+    Query the Reddit API and return the number of subscribers.
+
     Args:
         subreddit: name of the subreddit
-        
+
     Returns:
         Number of subscribers, or 0 if subreddit is invalid
     """
     if not subreddit or not isinstance(subreddit, str):
         return 0
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'CustomBot/1.0'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             return data.get('data', {}).get('subscribers', 0)
