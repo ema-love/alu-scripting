78bcc217f7476b6c97a782344f8e8fa58be35aca
Erioluwa <e.akintayo@alustudent.com>
2026-03-18 23:01:31 +0100

    @

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Returns number of subscribers for a subreddit"""
+"""Returns number of subscribers for a given subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyBot/1.0"}
+    """Query Reddit API for subscriber count."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyRedditBot/1.0 (by /u/ema_alx)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
-    return response.json().get("data", {}).get("subscribers", 0)
+    data = response.json()
+    return data.get("data", {}).get("subscribers", 0)
